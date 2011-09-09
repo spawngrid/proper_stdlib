2f2c3c15bf35abddddb2c2e3f7e5842a1b569724
Yurii Rashkovskii <yrashk@spawngrid.com>
2011-09-09 13:34:57 -0700

    Add initial simplistic support for email generation

--- src/proper_stdgen.erl
+++ src/proper_stdgen.erl
@@ -44,3 +44,94 @@ posix_filepath() ->
 posix_abs_filepath() ->
     frequency([{10, "/"++posix_filepath()}, {1, "/"}]).
 
+local_part_non_special_char() ->
+    oneof([
+           integer(65,90), %% a-z
+           integer(97,122), %% A-Z
+           integer(48,57), %% 0-9
+           %% !#$%&'*+-/=?^_`{|}~ 
+           33, 
+           integer(35,39), 
+           42, 
+           43, 
+           45, 
+           47, 
+           61, 
+           63, 
+           integer(94,96), 
+           integer(123,126)]).
+
+local_part_slash_special_char() ->
+    [$\\, oneof([32, 92, 34])].
+
+local_part_special_char() ->
+    %% (),:;<>@[]
+    oneof([
+           local_part_slash_special_char(),
+           40, 41, 44, 58, 59, 60, 62, 64, 91, 93
+          ]).
+
+local_part_maybe_special_char_list() ->
+    [$", non_empty(list(
+                     frequency([
+                                {10, local_part_non_special_char()},
+                                {5, local_part_special_char()}
+                           ])
+                    )), $"].
+
+email_local_part() ->
+    ?LET(LocalPart, 
+         ?SUCHTHAT(LocalPart,
+                   frequency([{100,
+                               non_empty(list(
+                                           frequency([
+                                                      {100, local_part_non_special_char()},
+                                                      {2, $.},
+                                                      {1, [$., local_part_maybe_special_char_list(), $.]}
+                                                     ])))},
+                              {1, 
+                               local_part_maybe_special_char_list()}]),
+                   %% rules for dots
+                   begin
+                       LocalPartF = lists:flatten(LocalPart),
+                       ((string:chr(LocalPartF, $.) > 1 andalso
+                         string:rchr(LocalPartF, $.) < length(LocalPartF))
+                        orelse
+                        string:chr(LocalPartF, $.) =:= 0) andalso
+                           string:str(LocalPartF, "..") =:= 0
+                   end),
+         lists:flatten(LocalPart)).
+                  
+
+label() ->
+    ?SUCHTHAT(Label,
+              non_empty(list(oneof([
+                                    integer(65,90), %% a-z
+                                    integer(97,122), %% A-Z
+                                    integer(48,57), %% 0-9,
+                                    $-]))),
+              ((string:chr(Label, $-) > 1 andalso
+                string:rchr(Label, $-) < length(Label)) orelse
+               string:chr(Label, $-) =:= 0) andalso
+              length(Label) < 64).
+
+email_domain() ->
+    frequency([{20,
+                ?SUCHTHAT(Hostname,
+                          ?LET(Labels,
+                               non_empty(list(label())),
+                               string:join(Labels, ".")),
+                          length(Hostname) < 256)},
+               {1, 
+                ?LET({A,B,C,D}, {integer(0,255),integer(0,255),integer(0,255),integer(0,255)},
+                     "[" ++ string:join([integer_to_list(A),
+                                         integer_to_list(B),
+                                         integer_to_list(C),
+                                         integer_to_list(D)],".") ++ "]")}
+               %% TODO: add support for IPv6
+               ]).
+
+email() ->
+    ?LET({LocalPart, Domain}, {email_local_part(), email_domain()},
+         string:join([LocalPart, Domain], "@")).
+
